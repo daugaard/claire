8aaceb4f8de8d0b963152f5b1f582d601c10e0a6
Soren Daugaard <sbd@ineptum.dk>
2017-12-06 20:31:39 -0500

    Added new posible model type

--- clairelib/HomeState.py
+++ clairelib/HomeState.py
@@ -11,6 +11,7 @@ class HomeState(Document):
     periodic_update = BooleanField()
     devices = ListField(DictField())
 
+    # Model 1
     def feature_vector(self):
         feature_vector = []
 
@@ -32,6 +33,7 @@ class HomeState(Document):
 
         return feature_vector
 
+    # Model 2
     def feature_vector_for_output_device(self, output_device):
         feature_vector = []
 
@@ -48,6 +50,30 @@ class HomeState(Document):
         # Loop over all devices sorted by ID - this will ensure the same order of features for every feature vector
         for device in sorted(self.devices, key=lambda d: d['device_id']):
             if output_device['device_id'] != device['device_id']:
+                device_feature_vector = self.feature_vector_device(device)
+                if device_feature_vector != None:
+                    feature_vector.extend(device_feature_vector)
+
+        return feature_vector
+
+    # Model 3
+    def feature_vector_for_output_device_in_same_location(self, output_device):
+        feature_vector = []
+
+        # Ensure all times are in UTC when creating feature vectors
+        utctime = datetime.utcfromtimestamp( self.time.timestamp() )
+
+        # Append with day of week
+        feature_vector.append( utctime.weekday() )
+
+        # Append with hour and minute number in 15 minutes interval
+        feature_vector.append( utctime.hour )
+        feature_vector.append( utctime.minute/15 )
+
+        # Loop over all devices sorted by ID - this will ensure the same order of features for every feature vector
+        for device in sorted(self.devices, key=lambda d: d['device_id']):
+            print(device)
+            if output_device['device_id'] != device['device_id'] and output_device['location'] == device['location']:
                 device_feature_vector = self.feature_vector_device(device)
                 if device_feature_vector != None:
                     feature_vector.extend(device_feature_vector)
@@ -88,7 +114,7 @@ class HomeState(Document):
         if device['type'] == 'BinarySensorDevice':
             return [1 if device['state'] > 1 else 0]
         elif device['type'] == 'MultiSensorDevice':
-            return [1 if device['state'] > 1 else 0, device['lux']]
+            return [1 if device['state'] > 1 else 0, device['lux']/2]
         elif device['type'] == 'BinaryPowerSwitchDevice':
             return [device['power_state']]
         else: # Other devices does not generate a feature vector
